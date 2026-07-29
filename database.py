447da70f541e66bf850bb5d5094d01acb2fa9d57
Rajnish <rajnish92006@gmail.com>
2026-07-29 22:34:02 +0530

    almost the v1 is near to complete without ui

--- database.py
+++ database.py
@@ -1,6 +1,7 @@
 #this is simple database code that will help me in handilng the database command
 import sqlite3
 import os
+from typing import Any
 
 ## this will use to intizlie the database command
 def intializeDatabase():
@@ -9,17 +10,39 @@ def intializeDatabase():
     if "database.db" not in os.listdir():
         connectDatabase = sqlite3.connect('database.db')
         cur = connectDatabase.cursor()
-        cur.execute("CREATE TABLE Transactions (transaction_date TEXT, withdrawal FLOAT, deposit FLOAT,catogrey TEXT,source TEXT)")
+        cur.execute("CREATE TABLE Transactions (unique_id INT,transaction_date TEXT, withdrawal FLOAT, deposit FLOAT,catogrey TEXT,source TEXT)")
         connectDatabase.commit()
+        cur.execute('''
+            CREATE TABLE IF NOT EXISTS config (
+                key TEXT PRIMARY KEY,
+                value FLOAT
+            )
+        ''')
+        connectDatabase.commit()
+        x = ["PNB","SBI","Cash"]
+        for i in x:
+            set_config(i,0.0)
         connectDatabase.close()
     
     connectDatabase = sqlite3.connect('database.db')
     cur = connectDatabase.cursor()
 
+def set_config(key, value):
+    cur.execute('''
+        INSERT OR REPLACE INTO config (key, value) 
+        VALUES (?, ?)
+    ''', (key, value))
+    connectDatabase.commit()
+
+def get_config(key):
+    cur.execute('SELECT value FROM config WHERE key = ?', (key,))
+    row = cur.fetchone()
+    return row[0]
+
 ## this will help in adding the tranction in database
 def addTransactions(transaction : tuple):
     try:
-        cur.execute("INSERT INTO Transactions VALUES (?,?,?,?,?)",transaction)
+        cur.execute("INSERT INTO Transactions VALUES (?,?,?,?,?,?)",transaction)
         connectDatabase.commit()
     except:
         print("Error...")
@@ -33,16 +56,15 @@ def deleteTransactions(transaction : tuple):
         print("item does not exist...")
 
 ## this will return the whole database
-def showAllTransactions():
-    cur.execute("SELECT rowid, * FROM Transactions")
+def showAllTransactions(lastDays : int = -1) -> list[Any]:
+    if(lastDays == -1):
+        cur.execute("SELECT * FROM Transactions ORDER BY unique_id DESC")
+    else:
+        q = f"SELECT * FROM Transactions ORDER BY unique_id DESC LIMIT {lastDays}"
+        cur.execute(q)
     transaction = cur.fetchall()
     return transaction
 
 ## close the connection from the database
 def closeDatabase():
     connectDatabase.close()
-
-def weekTransactions():
-    cur.execute("SELECT rowid, * FROM Transactions ORDER BY rowid DESC LIMIT 7")
-    transaction = cur.fetchall()
-    return transaction
--- main.py
+++ main.py
@@ -25,23 +25,24 @@ insights = tk.Frame(mainWindow,bg = "yellow",height=100)
 insights.pack(fill=tk.X,expand=True,anchor="n")
 insights.pack_propagate(False)
 
-def createInsights(textL : str) -> tk.Frame: 
+def createInsights(textL : str) -> tk.Label: 
     insights_i = tk.Frame(insights, bg = "white",width=300)
     insights_i.pack(fill=tk.Y,anchor="w",side="left",expand=True)
     insights_i.pack_propagate(False)
     tk.Label(insights_i,text=textL).pack()
-    return insights_i
+    x = tk.Label(insights_i,text=0.0)
+    x.pack()
+    return x
 
 labelInsights = ["PNB","Cash","SBI","Total"]
-frameInsights = []
+insightsLabel = []
 for eachLabel in labelInsights:
-    frameInsights.append(createInsights(eachLabel))
+    insightsLabel.append(createInsights(eachLabel))
 
-def updateInsights(details : tuple = (0,0,0)):
-    if details == (0,0,0):
-        print("hello")
-
-updateInsights()
+def updateInsights():
+    for i in range(0,3):
+        insightsLabel[i].configure(text = database.get_config(labelInsights[i]))
+    insightsLabel[3].configure(text = (database.get_config(labelInsights[0]) + database.get_config(labelInsights[1]) + database.get_config(labelInsights[2])))
 
 header = ["Transaction Date", "Withdrawal", "Deposit", "Catogrey", "Source",""]
 
@@ -80,18 +81,12 @@ def on_leave(event):
     is_mouse_over_canvas = False
 
 def mouse_scroll(event):
-    if(is_mouse_over_canvas):
+    if(is_mouse_over_canvas and len(trans) > 11):
         canvas.yview_scroll(int(-1*(event.delta/120)), "units")
 
 canvas.bind_all("<MouseWheel>", mouse_scroll)
 canvas.bind("<Enter>", on_enter)
 canvas.bind("<Leave>", on_leave)
-
-for i in range(0,25):
-    x = tk.Frame(transFrame,height=32,bg="pink")
-    x.pack(fill=tk.X, padx=4,pady=8)
-    x.pack_propagate(False)
-    tk.Label(x,text=i+1).pack()
 
 functionBtn = tk.Frame(mainWindow,bg = "yellow",height=50)
 functionBtn.pack(fill=tk.X,expand=True,anchor="s")
@@ -105,6 +100,8 @@ def createButton(textL : str) -> tk.Button:
 def delteteTop():
     add_trans.config(state="normal")
     top.destroy()
+    root.lift()
+    root.focus_force()
 
 def addthevalue():
     try:
@@ -128,7 +125,20 @@ def addthevalue():
     if(deposite.get() == 0 and withdrawal.get() == 0):  
         messagebox.showerror(title="Invalid Tranctions",message="Both cannot be zero at ones.")
         return
-    database.addTransactions((dateTranstion.get(),float(withdrawal.get()),float(deposite.get()),catogrey.get(),choosedoptions.get()))
+    if withdrawal.get() != 0:
+        if(database.get_config(choosedoptions.get())-withdrawal.get() < 0):
+            messagebox.showerror(title="Invalid Tranctions",message="Insufficent balnce")
+            return 
+        else:
+            database.set_config(choosedoptions.get(),database.get_config(choosedoptions.get())-withdrawal.get())
+    else:
+        database.set_config(choosedoptions.get(),database.get_config(choosedoptions.get())+deposite.get())
+    now = datetime.now()
+    z = now.strftime("%H%M%S")
+    y = datetime.strptime(dateTranstion.get(), "%d/%m/%Y").strftime("%Y%m%d")
+    database.addTransactions((int(y+z),dateTranstion.get(),float(withdrawal.get()),float(deposite.get()),catogrey.get(),choosedoptions.get()))
+    fetchTranction()
+    updateInsights()
     delteteTop()      
     
 def addTrans():
@@ -146,7 +156,8 @@ def addTrans():
     top.resizable(False,False)
     tk.Label(top,text="Transaction Date").grid(row=0,column=0)
     global dateTranstion
-    dateTranstion = tk.StringVar(value="DD/MM/YYYY")
+    now = datetime.now()
+    dateTranstion = tk.StringVar(value=now.strftime("%d/%m/%Y"))
     dateTrans = tk.Entry(top,textvariable=dateTranstion)
     dateTrans.grid(row=0,column=1)
     tk.Label(top,text="Withdrawal").grid(row=1,column=0)
@@ -173,13 +184,123 @@ def addTrans():
     sour.grid(row=4,column=1)
     submit = tk.Button(top,text="Add",command=addthevalue)
     submit.grid(row=5)
+    top.update_idletasks()
+    top.lift()    
+    top.focus_force()
+    dateTrans.focus_set()
     top.protocol("WM_DELETE_WINDOW", delteteTop)
     top.mainloop()
+
+def delteteTop1():
+    top1.destroy()
+    root.lift()
+    root.focus_force()
+    updateInsights()
+
+def addtheblance(): 
+    try:
+        float(pnbcash.get())
+        float(sbicash.get())
+        float(cashcash.get())
+    except:
+        messagebox.showerror(title="Invalid Format",message="Amount should be decimal or intger in ruppes.")
+        return
+    database.set_config("PNB",pnbcash.get())
+    database.set_config("SBI",sbicash.get())
+    database.set_config("Cash",cashcash.get())
+    delteteTop1() 
+
+def addBlance():
+    app_width = 250
+    app_height = 180
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+
+    x = (screen_width - app_width) / 2
+    y = (screen_height - app_height) / 2
+    global top1
+    top1 = tk.Toplevel()
+    top1.geometry(f"{app_width}x{app_height}+{int(x)}+{int(y)}")
+    top1.resizable(False,False)
+    top1.title("Opening Blance!!")
+    tk.Label(top1,text="PNB").grid(row=0,column=0)
+    global pnbcash
+    pnbcash = tk.DoubleVar(value=0.0)
+    withd = tk.Entry(top1,textvariable=pnbcash)
+    withd.grid(row=0,column=1)
+    tk.Label(top1,text="SBI").grid(row=1,column=0)
+    global sbicash
+    sbicash = tk.DoubleVar(value=0.0)
+    withd1 = tk.Entry(top1,textvariable=sbicash)
+    withd1.grid(row=1,column=1)
+    tk.Label(top1,text="Cash").grid(row=2,column=0)
+    global cashcash
+    cashcash = tk.DoubleVar(value=0.0)
+    withd11 = tk.Entry(top1,textvariable=cashcash)
+    withd11.grid(row=2,column=1)
+    submit = tk.Button(top1,text="Add",command=addtheblance)
+    submit.grid(row=3)
+    top1.update_idletasks()
+    top1.lift()    
+    top1.focus_force()
+    withd.focus_set()
+    top1.protocol("WM_DELETE_WINDOW", delteteTop1)
+    top1.mainloop()
+
+def fetchTranction():
+    x = 0
+    for i in range(0,len(labelInsights)-1):
+        x += int(database.get_config(labelInsights[i]))
+    global trans
+    trans = database.showAllTransactions()
+    if x == 0 and len(trans) == 0:
+        addBlance()
+        return
+    for widget in transFrame.winfo_children():
+        widget.destroy()
+    pnb_balance = 0
+    sbi_balance = 0
+    cash_blance = 0
+    if(len(trans) == 0):
+        print("No tranction")
+    else:
+        for item in reversed(trans):
+            if(item[5] == "PNB"):
+                if(item[2] != 0):
+                    pnb_balance -= item[2]
+                else:
+                    pnb_balance += item[3]
+            elif(item[5] == "SBI"):
+                if(item[2] != 0):
+                    sbi_balance -= item[2]
+                else:
+                    sbi_balance += item[3]
+            else:
+                if(item[2] != 0):
+                    cash_blance -= item[2]
+                else:
+                    cash_blance += item[3]
+        for item in trans:
+            x = tk.Frame(transFrame,height=32,bg="pink")
+            x.pack(fill=tk.X, padx=4,pady=8)
+            x.pack_propagate(False)
+            for i in range(1,len(item)):
+                y = tk.Frame(x,width=190)
+                y.pack(side="left",fill=tk.Y,expand=True,padx=(10,0),anchor="w")
+                y.pack_propagate(False)
+                tk.Label(y,text=item[i]).pack()
+            y = tk.Frame(x,width=190)
+            y.pack(side="left",fill=tk.Y,expand=True,padx=(10,0),anchor="w")
+            y.pack_propagate(False)
+            tk.Label(y,text="Delete").pack()
+    updateInsights()
 
 add_trans = createButton("Add Tranction")
 add_trans.configure(command=addTrans)
 fetch_trans = createButton("Fetch Tranction")
+fetch_trans.config(command=fetchTranction)
 
 database.intializeDatabase()
+fetchTranction()
 
 root.mainloop()
